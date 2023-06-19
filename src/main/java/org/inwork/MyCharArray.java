2b55584a70f9084762dd86443ab10c2656298358
oleg <legik7127@gmail.com>
2023-06-19 17:18:55 +0600

    added boolean parameter in methods checkSizeArray(), checkDataArray()

--- src/main/java/org/inwork/MyCharArray.java
+++ src/main/java/org/inwork/MyCharArray.java
@@ -16,7 +16,8 @@ public class MyCharArray {
 
 //    protected char[][] myCharArray = new char[4][4];
 
-    public void checkSizeArray(Character[][] myCharArray) {
+    public boolean checkSizeArray(Character[][] myCharArray) {
+        boolean b = true;
         try {
             if (myCharArray.length != 4) {
                 size = myCharArray.length;
@@ -24,7 +25,7 @@ public class MyCharArray {
             }
             if (myCharArray[0].length != 4) {
                 size = myCharArray[0].length;
-                throw new MyArraySizeException("Число столбов не соответствует 4-м", size);
+                throw new MyArraySizeException("Число столбцов не соответствует 4-м", size);
             }
             for (int i = 0; i < myCharArray.length; i++) {
                 for (int j = 0; j < myCharArray[0].length; j++) {
@@ -36,32 +37,41 @@ public class MyCharArray {
             }
 
         } catch (MyArraySizeException ex) {
+            b = false;
             System.out.printf("сообщение: %s, текущее количество: %d\n", ex.getMessage(), ex.getSize());
 
         } catch (ArrayIndexOutOfBoundsException ex) {
+            b = false;
             System.out.println("Не все элементы присутствуют, массив должен быть 4x4");
         }
 
+        return b;
+
     }
 
-    public void checkDataArray(Character[][] myCharArray) {
-        int[][] intAr = new int[4][4];
+    public void checkDataArray(Character[][] myCharArray, boolean b) {
+        int y =  4;
+        int x =  4;
+        int[][] intAr = new int[x][y];
         int sum = 0;
-        try {
-            for (int i = 0; i < intAr.length; i++) {
-                for (int j = 0; j < intAr[0].length; j++) {
-                    intAr[i][j] = Character.digit(myCharArray[i][j], 10);
-                    if (!Character.isDigit(myCharArray[i][j])) {
-                        throw new MyArrayDataException(getClass(), "Данный элемент не число", i, j, myCharArray[i][j]);
+        if (b == true) {
+
+            try {
+                for (int i = 0; i < x; i++) {
+                    for (int j = 0; j < y; j++) {
+                        if (!Character.isDigit(myCharArray[i][j])) {
+                            throw new MyArrayDataException(getClass(), "Данный элемент не число", i, j, myCharArray[i][j]);
+                        }
+                        intAr[i][j] = Character.digit(myCharArray[i][j], 10);
+                        sum += intAr[i][j];
                     }
-                    sum += intAr[i][j];
                 }
+            } catch (MyArrayDataException ex) {
+                System.out.printf("сообщение: %s - '%c' - координаты элемента: i = %d, j = %d \n",
+                        ex.getMessage(), ex.getC(), ex.getX(), ex.getY());
+            } finally {
+                System.out.printf("Сумма элементов равна %d: ", sum);
             }
-        } catch (MyArrayDataException ex) {
-            System.out.printf("сообщение: %s - '%c' - координаты элемента: i = %d, j = %d \n",
-                    ex.getMessage(), ex.getC(), ex.getX(), ex.getY());
-        } finally {
-            System.out.printf("Сумма элементов равна %d: ", sum);
         }
 
     }
--- src/main/java/org/inwork/Program.java
+++ src/main/java/org/inwork/Program.java
@@ -4,31 +4,17 @@ import java.util.Arrays;
 
 public class Program {
     public static void main(String[] args) {
-        Character[][] ch = new Character[][] {
-                {'1','2','3','4'},
-                {'1','2','3','4'},
-                {'1','2','k','4'},
-                {'1','2','3','4'},
+        Character[][] ch = new Character[][]{
+                {'1', '2', '3', '4'},
+                {'1', '2', 'k', '4'},
+                {'1', '2', '3', '4'},
+                {'1', '2', '3', '4'},
+
         };
 
         System.out.println();
         MyCharArray myCharArray = new MyCharArray();
-//        myCharArray.checkSizeArray(ch);
-        myCharArray.checkDataArray(ch);
-
-//        int[][] arInt = new int [4][4];
-//        for (int i = 0; i < ch.length; i++) {
-//            for (int j = 0; j < ch[0].length; j++) {
-//                arInt[i][j] = Character.digit(ch[i][j], 10);
-//                if (!Character.isDigit(ch[i][j])){
-//                  break;
-//                }
-////              System.out.printf("строка: %d, столбец :%d", i, j);
-//               System.out.print(arInt[i][j]);
-//
-//            }
-//            System.out.println();
-//        }
+        myCharArray.checkDataArray(ch, myCharArray.checkSizeArray(ch));
 
     }
 }
